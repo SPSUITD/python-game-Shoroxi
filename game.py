e29f0d9813fe32564df85c3471dbafc4a613f716
Шкарупа Глеб <mspoxir@gmail.com>
2023-05-25 02:13:59 +0300

    pause menu

--- game.py
+++ game.py
@@ -26,15 +26,10 @@ game_session = None
 pause = True
 # menu = False
 
-hiss = Audio(sound_folder + "spider_hiss", volume=5, autoplay=False, loop=False)
+# hiss = Audio(sound_folder + "spider_hiss", volume=5, autoplay=False, loop=False)
 
-goblin = Entity(position=(0, -40, 90), rotation=Vec3(0,180,0), scale=14)
-goblin.animate_position()
-actor = Actor(anim_folder + "goblin.gltf")
-actor.reparent_to(goblin)
-
-video = anim_folder + 'video.mp4'
-video_player = Entity(model='quad', parent=camera.ui, scale=(1.5, 1), texture=video)
+# video = anim_folder + 'video.mp4'
+# video_player = Entity(model='quad', parent=camera.ui, scale=(1.5, 1), texture=video)
 
 
 def check_itm(itm):
@@ -55,6 +50,19 @@ def get_player():
             return game_session.player
     else:
         return None
+
+def get_enemy():
+    if game_session:
+        if game_session.enemy is not None:
+            return game_session.enemy
+    else:
+        return None
+
+def get_trigger(id):
+    for trigger in game_session.current_level.level_objects:
+        if trigger.name == "trigger":
+            if trigger.trigger_id == id:
+                return trigger
 
 def set_current_status(trigger):
     if game_session:
@@ -123,18 +131,21 @@ class Player(Entity):
         self.fall_after = .3
         self.jumping = False
         self.air_time = 0
+        # ------------  ---------------
         self.inventory = Inventory()
+        self.pause_menu = ui.GamePause(enabled=False)
         # ------------ Ray ---------------
         # self.hit_text = "None"
         self.ray_hit = raycast(self.position + (0, 20, 0), self.direction, ignore=(self,), distance=100, debug=False)
         # ------------ Ui ---------------
         self.press_e = ui.UIText("press [E]", parent=camera.ui,offset=(0.0018,0.0018), y=-0.35, enabled=False, color=color.white,origin=(0,0))
         self.fps_counter = ui.UIText("", (0.0018, 0.0018), color=setting.color_orange, position=(window.right.x - 0.13, window.top.y - .1))
-        self.cursor = Sprite(texture="quad", parent=camera.ui, scale=.005, color=color.white)
+        self.cursor = Entity(model="quad", parent=camera.ui, scale=.005, color=color.white)
         self.crosshair_tip_text = ""
         self.crosshair_tip = ui.UIText(parent=camera.ui, offset=(0.0015,0.0015), text=self.crosshair_tip_text, origin=(0, 0), y=0.04,
                                        color=color.white, scale=1, x=0, z=-0.001)
         self.msg = ui.UIText("", origin=(-.5, 0),offset=(0.0015, 0.0015), parent=camera.ui, position=(window.left.x+0.02, 0), color=color.orange)
+        self.gameover_screen = ui.GameOverScreen(enabled=False)
         # ------------ Status ---------------
         self.trigger_status = ""
         # ------------ Mouse ---------------
@@ -157,29 +168,25 @@ class Player(Entity):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def load_location(self,level):
+    def load_location(self):
         camera.overlay.color = color.black
         loading = Text("loading", origin=(0, 0), color=setting.color_orange, always_on_top=True)
 
         set_player_to_level_spawn_point()
         self.set_crosshair()
         self.raycast_once()
-        invoke(setattr, camera.overlay, 'color', color.clear, delay=2)
-        destroy(loading, delay=2)
-        # TODO ADD fade
+        destroy(loading)
+        invoke(setattr, camera.overlay, 'color', color.clear, delay=1)
 
     def raycast_once(self):
         self.ray_hit = raycast(self.position + (0, 20, 0), self.direction, ignore=(self,), distance=100, debug=False)
-
-    def set_player_pos(self,x,y,z):
-        self.position = (x,y,z)
 
     def get_player_pos(self):
         return self.position
 
     def set_crosshair(self):
         self.press_e.enable()
-
+    # ------------- Gravity --------------
     def jump(self):
         if not self.grounded:
             return
@@ -195,6 +202,7 @@ class Player(Entity):
         self.air_time = 0
         self.grounded = True
 
+    # -----------------------------------
     def input(self, keypress):
         global gameplay
         global pause
@@ -262,10 +270,11 @@ class Player(Entity):
 
                             self.crosshair_tip_text = ""
 
-            if keypress == "escape" and not self.dialogue.enabled:
-                # TODO: pause menu
+            if keypress == "escape":
+                pause = True
+                invoke(self.pause_menu.enable, delay=0.001)
                 # pause = True
-                pass
+                # ui.GameOverScreen()
 
     # ФУНКЦИЯ ОБНОВЛЕНИЯ ДЛЯ КАЖДОГО КАДРА
     def update(self):
@@ -331,7 +340,7 @@ class Player(Entity):
 
                 # Interact
                 self.direction = Vec3(camera.forward)
-                self.ray_hit = raycast(self.position + (0, 24, 0), self.direction, ignore=(self,), distance=20, debug=True)
+                self.ray_hit = raycast(self.position + (0, 24, 0), self.direction, ignore=(self,), distance=20, debug=False)
                 # Camera
                 self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1]
                 self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity[0]
@@ -340,6 +349,7 @@ class Player(Entity):
                 self.direction_move = Vec3(
                     self.forward * (held_keys['w'] - held_keys['s']) + self.right * (held_keys['d'] - held_keys['a'])).normalized()
 
+                # Collider
                 feet_ray = raycast(self.position + Vec3(0, 3, 0), self.direction_move, ignore=(self,), distance=4, debug=False)
                 head_ray = raycast(self.position + (0, 24, 0), self.direction_move, ignore=(self,), distance=4, debug=False)
 
@@ -374,6 +384,23 @@ class Player(Entity):
                 self.y -= min(self.air_time, ray.distance - .05) * time.dt * 100
                 self.air_time += time.dt * .25 * self.gravity
 
+class Enemy(Entity):
+
+    def __init__(self, **kwargs):
+        super().__init__(
+            position=(50, -30, -60),
+            rotation=Vec3(0,90,0),
+            scale=14
+        )
+        self.actor = Actor(anim_folder + "goblin.gltf")
+        self.actor.reparent_to(self)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        # other logics
+        # ray-cast
+        # memory
+        # path-searcher
 
 class Trigger(Entity):
     def __init__(self, **kwargs):
@@ -399,15 +426,23 @@ class Trigger(Entity):
 
     def update(self):
         global pause
-        # print(goblin.position)
+
         self._i += 1
         if self._i < self.update_rate:
             return
         self._i = 0
 
+        # проверка каждый кадр
         if get_current_status() == "enemy":
-            # pause = True
-            print("Вошел в триггер: " + self.get_trigger_id())
+            pause = True
+            # Победа
+            if check_itm("pistol"):
+                video_player.play()
+                # video_sound.play()
+                pass
+            # Проигрыш
+            else:
+                get_player().gameover_screen.enabled = True
 
         for other in self.trigger_targets:
             if other == self:
@@ -422,21 +457,16 @@ class Trigger(Entity):
                 set_current_status(self.get_trigger_id())
 
                 if get_current_status() == "door_enter":
+
                     if check_itm("pistol"):
-                        # pre-render anim
-                        video_sound.play()
-                        # finish
-                        pass
-                    else:
-                        # dead
-                        # menu
-                        pass
+                        get_trigger("enemy").animate_position(value=(-40, -44, 168), duration=0.5, curve=curve.linear)
+                        get_enemy().actor.play("jump")
 
                     if not check_itm("light"):
                         get_player().msg.setText("Включите свет")
 
                 if get_current_status() == "cass":
-                    if not check_itm("trash"):
+                    if not check_itm("trash") and check_itm("light"):
                         get_player().msg.setText("Соберите и выбросите мусор")
 
                     if check_itm("pistol"):
@@ -464,15 +494,13 @@ class Trigger(Entity):
 
                 if get_current_status() == "window":
                     # if self.check_itm("tovar"):
-
-                    actor.loop("jump")
                     get_player().msg.setText("Представьте что вы взяли пистолет из под прилавка")
 
-                # if get_current_status() == "cass":
-                #     if check_itm("tovar"):
-                #         add_itm("pistol")
-                #     if check_itm("pistol"):
-                #         get_player().msg.setText("Проверьте кто это был")
+                if get_current_status() == "cass":
+                    if check_itm("tovar"):
+                        add_itm("pistol")
+                    if check_itm("pistol"):
+                        get_player().msg.setText("Проверьте кто это был")
 
                 continue
 
@@ -535,22 +563,20 @@ class Gameplay(Entity):
         pause = False
         game_session = self
         self.player = Player()
-        # передаём ссылку с созданным гг в уровень для дальнейшего доступа к классу гг
-        invoke(self.player.load_location, player_creature["start_level"] if level is None else level,delay=0.00001)
+        self.enemy = Enemy()
+
+        # передаём ссылку с созданным гг в уровень для дальнейшего доступа к классу гг (получаем какой уовень на данный момент нужно запускать)
+        invoke(self.player.load_location,delay=0.00001)
         self.current_level = Level(self.player, level_id=player_creature["start_level"] if level is None else level)
 
-        # for i in game_session.current_level.level_objects:
-        #     pprint.pprint(i)
         set_player_to_level_spawn_point()
-
         gameplay = True
         self.player.msg.setText("Зайдите в свой магазин")
-
 
         # video_sound = ursina.loader.loadSfx(video)
         # video_player.texture.synchronizeTo(video_sound)
 
-        Audio(sound_folder+"amb", volume=0.1, autoplay=False, loop=True).play()
+        # Audio(sound_folder+"amb", volume=0.1, autoplay=False, loop=True).play()
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -639,6 +665,11 @@ class Level(Entity):
                             scale=trigger["radius"],
                             trigger_id=trigger["id"],
                             id=trigger["id"] if "id" in trigger else None)
+
+                if "id" in trigger:
+                    if trigger["id"] == "enemy":
+                        get_enemy().reparent_to(t)
+
                 t.keys = trigger
                 self.level_objects.append(t)
 
@@ -674,7 +705,6 @@ class Level(Entity):
                     l.keys = light
                     self.level_objects.append(l)
 
-
 class LevelObject(Entity):
     def __init__(self, **kwargs):
         super().__init__()
@@ -686,3 +716,5 @@ class LevelObject(Entity):
 
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+
